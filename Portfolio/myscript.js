c488353825828b707017792beb4264bd0318d7a4
Drew <akrytical@yahoo.com>
2017-03-30 19:43:41 -0700

    Alpha 1

---
+++ Portfolio/myscript.js
@@ -0,0 +1,9 @@
+function myFunction(){
+	var text = document.getElementById("demo")
+	text.innerHTML = "Hello JavaScript!";
+	var px = document.getElementById("demo")
+	px.style.fontSize = "25px"; 
+	var red = document.getElementById("demo")
+	red.style.color = "red";      
+}
+
